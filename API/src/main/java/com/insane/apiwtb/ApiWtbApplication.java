b716ad87baedd2b8dbcea3322e81bced19a3c7f5
Renne Castrucci <rennecastrucci@gmail.com>
2023-02-24 10:59:20 -0300

    Spring Initializr Maven Project para API

---
+++ API/src/main/java/com/insane/apiwtb/ApiWtbApplication.java
@@ -0,0 +1,13 @@
+package com.insane.apiwtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWtbApplication.class, args);
+	}
+
+}
---
+++ API/src/test/java/com/insane/apiwtb/ApiWtbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insane.apiwtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWtbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
