828bb2cab461210fc12d6ba92e6b61ec0697f48b
Renne Castrucci <rennecastrucci@gmail.com>
2023-02-26 02:38:38 -0300

    API - Implemented Favourite Dresses, Blouses and Bra

--- API/src/main/java/com/insane/apiwtb/model/User.java
+++ API/src/main/java/com/insane/apiwtb/model/User.java
@@ -49,4 +49,16 @@ public class User {
     @JoinColumn(name = "id_image")
     private Image image;
 
+    @ManyToMany
+    @JoinTable(name = "tb_favourite_dress", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_dress"))
+    private List<Blouse> favouriteDresses  = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_favourite_blouse", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_blouse"))
+    private List<Blouse> favouriteBlouses  = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_favourite_bra", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_bra"))
+    private List<Blouse> favouriteBras  = new ArrayList<>();
+
 }
